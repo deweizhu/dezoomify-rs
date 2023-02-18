04364d5e179ef59b1aac68f495f67eff71add9ee
zhudw <zhudwi@foxmail.com>
2023-02-18 07:26:44 +0000

    update build

--- build.sh
+++ build.sh
@@ -2,3 +2,13 @@
 
 sudo docker run --rm -it -v $(pwd):/io -w /io messense/cargo-zigbuild   cargo zigbuild --release --target aarch64-apple-darwin
 sudo docker run --rm -it -v $(pwd):/io -w /io messense/cargo-zigbuild   cargo zigbuild --release --target x86_64-apple-darwin
+
+pwd=`pwd`
+cd ./target/x86_64-apple-darwin/release
+sudo tar -cjf dezoomify-rs_v2.9.4_macOS.tar.bz2 dezoomify-rs
+cd $pwd
+
+cd ./target/aarch64-apple-darwin/release
+sudo tar -cjf dezoomify-rs_v2.9.4_macOS_M2.tar.bz2 dezoomify-rs
+
+cd $pwd
